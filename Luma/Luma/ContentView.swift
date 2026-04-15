28f8ed5772ab7c2701d27372fda7141677d82930
Anastasiia Yefremova <yefremova.a.s@gmail.com>
2026-04-15 15:18:08 +0200

    initial setup

---
+++ Luma/Luma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Luma
+//
+//  Created by Anastasiia Yefremova on 15.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Luma/Luma/LumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumaApp.swift
+//  Luma
+//
+//  Created by Anastasiia Yefremova on 15.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct LumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
